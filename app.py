fdfe1e077e384b9d6cfd2ef2d3ecd88716677725
Srushith <srushithk@gmail.com>
2023-12-17 12:19:43 +0530

    The web app is ready

--- app.py
+++ app.py
@@ -1,30 +1,45 @@
 import pickle
-from flask import Flask,request,app,jsonify,render_template
+from flask import Flask, request, jsonify, render_template
 import numpy as np
-import pandas as pd
 
-
-app=Flask(__name__)
+app = Flask(__name__)
 # Load the model
-model=pickle.load(open('vehicle_pred_model1.pkl','rb'))
+model = pickle.load(open('vehicle_pred_model1.pkl', 'rb'))
+
+def convert_to_native_types(data):
+    converted_data = {}
+    for key, value in data.items():
+        try:
+            # Attempt to convert the value to a native Python type
+            converted_data[key] = np.asscalar(np.float64(value)) if isinstance(value, np.float32) else value
+        except Exception as e:
+            converted_data[key] = value
+    return converted_data
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route('/predict_api',methods=['POST'])
-
+@app.route('/predict_api', methods=['POST'])
 def predict_api():
-    data=request.json['data']  
-    for key, value in data.items():
-        if isinstance(value, np.float32):
-            data[key] = float(value)
-    print(data)
-    print(np.array(list(data.values())).reshape(1,-1))
-    new_data=np.array(list(data.values())).reshape(1,-1)
-    output=model.predict(new_data)
-    print(output[0])
-    return jsonify(float(output[0]))
+    # Access form data using request.form instead of request.json
+    data = request.form.to_dict()
+    data = convert_to_native_types(data)
 
-if __name__=="__main__":
+    # Adjust column names here according to your model
+    column_names = ['year', 'fuelConsump_per_100km', 'kilometers', 'brand', 'transmission', 'drive', 'fuel', 'body_style', 'seats', 'engine']
+
+    # Create a dictionary with default values for all columns
+    default_data = {col: None for col in column_names}
+
+    # Update default_data with received data
+    default_data.update(data)
+
+    # Example: Adjusting input data to match the expected 10 features
+    new_data = np.array(list(default_data.values()))[:10].reshape(1, -1)
+
+    output = model.predict(new_data)
+    return render_template('home.html', prediction_text=f"Predicted output: {output[0]}")
+
+if __name__ == "__main__":
     app.run(debug=True)
